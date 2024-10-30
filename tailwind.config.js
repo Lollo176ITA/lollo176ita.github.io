6609f7d57d8c093920ed60153b90886f56cabb39
Lollo176ITA <lollo176ita@gmail.com>
2024-10-30 16:11:10 +0100

    test

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // Indica che le classi usate in questo file devono essere compilate
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
