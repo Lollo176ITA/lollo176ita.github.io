709fdb8b635019975fd5af8492c65c10bccbd4ad
Lollo176ITA <lollo176ita@gmail.com>
2024-10-31 19:02:45 +0100

    test 3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Rimuove il prefisso aggiunto nel percorso del build
+  base: './', // Rimuove il prefisso aggiunto nel percorso del build
+  build: {
+    rollupOptions: {
+      input: 'index.html'  // Specifica il file originale di sviluppo
+    },
+    },
 });
