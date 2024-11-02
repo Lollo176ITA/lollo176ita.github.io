41dfd9e82e47fc396c56f2773032077a1c43a324
Lollo176ITA <lollo176ita@gmail.com>
2024-11-02 19:04:31 +0100

    refactor NotFoundPage component for improved structure and styling

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,17 +1,15 @@
 // src/pages/NotFoundPage.jsx
-import Navbar from '../components/NavBar';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
   return (
-    <div className="bg-black text-white min-h-screen">
-      <Navbar />
-      <div className="p-4 flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold mb-4">404</h1>
-        <p className="text-lg mb-4">Oops! La pagina che stai cercando non esiste.</p>
-        <Link to="/" className="text-blue-400 hover:text-blue-200 underline">
-          Torna alla Home
-        </Link>
-      </div>
+    <div className="bg-black text-white min-h-screen p-4 flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-lg mb-4">Pagina non trovata</p>
+      <Link to="/" className="text-blue-400 underline">Torna alla Home</Link>
     </div>
   );
 }
+
+export default NotFoundPage;
