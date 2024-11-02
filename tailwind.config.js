f004a892722148d80250f9b5e7462bd8d5e82ee3
Lollo176ITA <lollo176ita@gmail.com>
2024-11-02 19:13:42 +0100

    update Tailwind CSS configuration for simplified content paths and remove unused animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./**/*.{html,js,jsx,ts,tsx,vue,css}",  // Indica che le classi usate in ogni file con queste estensioni devono essere compilate
+    './index.html',
+    './src/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0, transform: 'scale(0.5)' },
-          '100%': { opacity: 1, transform: 'scale(1)' },
-        },
-      },
-      animation: {
-        fadeIn: 'fadeIn 2s ease-in-out',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
