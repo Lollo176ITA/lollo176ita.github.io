c467a5953ebccc7c28dbfdcd8ff7571c9c9f7257
Lollo176ITA <lollo176ita@gmail.com>
2024-11-03 12:10:38 +0100

    reset project

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // Importa Tailwind CSS
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
