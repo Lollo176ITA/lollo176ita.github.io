ceeefd525646b52d95a003847dc90ec5349a2c28
Lollo176ITA <lollo176ita@gmail.com>
2024-11-27 10:24:01 +0100

    chore:  adjust deployment configuration

--- wiki/svelte.config.js
+++ wiki/svelte.config.js
@@ -1,28 +1,18 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-
 	kit: {
 		adapter: adapter({
-			pages: 'build', // Directory dove vengono messi i file statici
-			assets: 'build', // Dove vengono messi i file di asset (CSS, JS, immagini)
-			fallback: null // Nessun file fallback
-		  }),
-		  prerender: {
-			handleMissingId: 'ignore' // Ignora i link a ID mancanti
-		  },
-		  paths: {
-			base: '/lollo176ita.github.io', // Nome del repository
-		  },
-	},
-
-	extensions: ['.svelte', '.svx']
+			fallback: '404.html'
+		}),
+		prerender: {
+			handleMissingId: 'ignore'
+		},
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
