8ee15048663d21d48aabd48f9b283a9227ffba2f
Lollo176ITA <lollo176ita@gmail.com>
2024-11-29 23:28:46 +0100

    fix: added link click handler to toggle menu in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar({ isOpen, toggleMenu }) {
+  const handleLinkClick = () => {
+    // Puoi aggiungere altra logica qui se necessario
+    toggleMenu();
+  };
+
   return (
     <div
       className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg transform ${
@@ -9,12 +14,18 @@ export default function Navbar({ isOpen, toggleMenu }) {
       } transition-transform duration-300 ease-in-out z-20`}
     >
       <button onClick={toggleMenu} className="absolute top-4 right-4 text-3xl focus:outline-none">
-        &times; {/* Close icon */}
+        &times; {/* Icona di chiusura */}
       </button>
       <ul className="flex flex-col p-8 space-y-6 mt-12">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
+        <li>
+          <Link to="/" onClick={handleLinkClick}>Home</Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={handleLinkClick}>About</Link>
+        </li>
+        <li>
+          <Link to="/projects" onClick={handleLinkClick}>Projects</Link>
+        </li>
       </ul>
     </div>
   );
