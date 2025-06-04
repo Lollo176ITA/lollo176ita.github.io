fe8df77d82ce6e5d319c2b781c3e34590a994cf1
Lollo176ITA <116870391+Lollo176ITA@users.noreply.github.com>
2025-06-04 10:10:40 +0200

    Fix comment to refer to LanguageSwitcher.js

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,4 +1,4 @@
-// src/components/LanguageSwitcher.jsx
+// src/components/LanguageSwitcher.js
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactCountryFlag from 'react-country-flag';
